ceaadc5d96aa5901b7c8db4129af839d8d9de905
SheaML <6855717+SheaML@users.noreply.github.com>
2018-09-19 00:08:17 -0700

    Update haplotype_resampling.sh
    typos, useless use of cat

--- haplotype_resampling.sh
+++ haplotype_resampling.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#This script for creating an alignment with one randomly sampled haplotype for each locus and individual, using a phased alignment containing two haplotypes per individual
+#This script is for creating an alignment with one randomly sampled haplotype for each locus and individual, using a phased alignment containing two haplotypes per individual
 #First, split a phylip file containing 2 haplotypes for each individual into separate files for each locus. I use RAXML v8.X and the "-f s" option for this.
 #Then, for each phylip file (containing haplotypes for 1 locus), we will sample a random haplotype for each individual. 
 #Individual names must be defined in the names.txt file
@@ -15,9 +15,9 @@ paste *.raw -d '' > all.concat.raw
 ##put names back 
 paste ../names.txt all.concat.raw -d' ' > all.concat.named
 ##get the new number of taxa and seqlen
-ntax=`cat all.concat.raw | wc -l`
+ntax=`wc -l all.concat.raw`
 let count=`awk 'NR==5' all.concat.named | awk '{print $2}' | wc -m`-1
-##replace headerd
+##replace header
 cat <(echo $ntax	$count) all.concat.named > all.concat.named.phy
 
 ##The above will create a single resampled replicate. 
